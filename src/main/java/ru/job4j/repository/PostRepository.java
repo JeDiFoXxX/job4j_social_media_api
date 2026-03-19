b57ba93052fe7814a206a70edc6c52e809d9dfcb
JeDiFoXxX <serjik1993991@gmail.com>
2026-03-19 19:12:06 +0300

    Добавление запросов | PostRepository

--- src/main/java/ru/job4j/repository/PostRepository.java
+++ src/main/java/ru/job4j/repository/PostRepository.java
@@ -1,6 +1,21 @@
 package ru.job4j.repository;
 
+import jakarta.persistence.QueryHint;
+import org.springframework.data.jpa.repository.QueryHints;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
 import ru.job4j.model.Post;
 
-public interface PostRepository extends JpaRepository<Post, Long> { }
+import java.time.Instant;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByUserId(Long userId);
+
+    List<Post> findByUserIdAndCreateAtBetween(Long userId, Instant after, Instant before);
+
+    Page<Post> findByUserIdOrderByCreateAtDesc(Long userId, Pageable pageable);
+}
